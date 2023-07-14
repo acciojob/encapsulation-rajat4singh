bddd8aa1eb27b01c5a98dd5e217030693dfd8e4c
Rajat <rs.sunny100@gmail.com>
2023-07-14 12:14:06 +0530

    code done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+   public static void main(String args[]) {
+        RWOnly obj = new RWOnly();
+
+        obj.setName("Rajat");
+
+       
+        System.out.println("Name: " + obj.getName());
+    }
 }
